bce040e850a00e72b2db8ad3d550db31314d5fe6
David Jones <Jones_David_S@yahoo.com>
2018-02-14 12:27:16 -0600

    fuck

---
+++ Assets/Scripts/ReadJSON.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class ReadJSON : MonoBehaviour
+{
+    private string JSONstring;
+    private JsonData testing;
+
+	// Use this for initialization
+	void Start ()
+    {
+        JSONstring = File.ReadAllText(Application.dataPath + "/Jsons/Change.json");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
